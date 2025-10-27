7b70e1343b641732002f846754e3bec95e63c849
kdoyleCS <kdoyle5267@sdsu.edu>
2025-10-26 21:46:27 -0700

    Finished downheap

--- heap.h
+++ heap.h
@@ -37,7 +37,24 @@ struct MinHeap {
     }
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        int parent = pos;
+        while (true) {
+            int left = 2 * parent + 1;
+            int right = 2 * parent + 2;
+            int smallest = parent;
+
+            if (left < size && weightArr[data[left]] < weightArr[data[smallest]]) {
+                smallest = left;
+            }
+            if (right < size && weightArr[data[right]] < weightArr[data[smallest]]) {
+                smallest = right;
+            }
+            if (smallest == parent) {
+                break;
+            }
+            swap(data[parent], data[smallest]);
+            parent = smallest;
+        }
     }
 };
 
